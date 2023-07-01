4c4e0367fc78f8b594c2e2478a6bb195ceeb2a13
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2023-07-01 16:49:06 +0600

    sili change

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="text-secondary mb-6 md:mb-0">
             <h3 className={`text-2xl font-semibold ${titleFont.className}`}>Stay Connected</h3>
-            <p className={`mt-2 ${textFont.className}`}>Follow us on social media for updates and news.</p>
+            <p className={`mt-2 ${textFont.className}`}>Follow me on social media for updates and news.</p>
           </div>
           <div className="flex space-x-4">
             <a href="https://www.facebook.com/Md.Nur9211/" target="_blank" className="text-secondary hover:text-secondaryLight transition duration-300">
